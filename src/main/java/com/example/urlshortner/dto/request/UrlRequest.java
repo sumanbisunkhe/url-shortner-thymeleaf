3be0238c85d5873f21593b7b855dc14f3f1b43b5
sumanbisunkhe <your-email@example.com>
2025-11-25 11:04:41 +0545

    fixed errors

--- src/main/java/com/example/urlshortner/dto/request/UrlRequest.java
+++ src/main/java/com/example/urlshortner/dto/request/UrlRequest.java
@@ -1,12 +1,8 @@
 package com.example.urlshortner.dto.request;
 
-<<<<<<< HEAD
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-=======
-import jakarta.validation.constraints.*;
->>>>>>> c8a73bf542437b4a82eb4ae49705ad7fcf75a231
 import lombok.Data;
 import lombok.Getter;
 import org.hibernate.validator.constraints.URL;
